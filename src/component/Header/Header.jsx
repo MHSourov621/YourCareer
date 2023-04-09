ab3b2a4c80a48a09f380a1b2af1d32b1a6251b07
MHSourov621 <mhsourov621@gmail.com>
2023-04-09 23:10:48 +0600

    header added in home section

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Header.css";
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='header-section'>
+            <h2 className='logo-name'>YourCareer</h2>
+            <div className='link-div'>
+                <Link to="/statistics">Statistics</Link>
+                <Link to="/jobs">Applied Jobs</Link>
+                <Link to="/blog">Blog</Link>
+            </div>
+            <button className='btn header-btn'>Start Applying</button>
+        </div>
+    );
+};
+
+export default Header;
